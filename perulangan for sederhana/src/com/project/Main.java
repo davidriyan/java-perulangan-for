4afc24d45ba0433de7e7374d56f447f93b450add
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-04-11 23:04:15 +0700

    Add files via upload

---
+++ perulangan for sederhana/src/com/project/Main.java
@@ -0,0 +1,9 @@
+package com.project;
+public class Main{
+    public static void main (String[]args){
+        //MASUKKAN LOOPING FOR-NYA
+        for (int a = 1; a <= 10; a++){
+            System.out.println(a);
+        }
+    }
+}
